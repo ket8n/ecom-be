a02f7eccfddeb3755e4510501c63cedf91c9908e
ket8n <ketanchopade.kc@gmail.com>
2025-05-28 22:41:17 +0530

    Add: basic configurations for project

---
+++ src/main/java/com/svarna/ecombe/EcomBeApplication.java
@@ -0,0 +1,13 @@
+package com.svarna.ecombe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomBeApplication.class, args);
+    }
+
+}
